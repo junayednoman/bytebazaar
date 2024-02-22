81aa5d38e14acd4cbe06d67a36c82b3d089cd358
junayednoman <junayednoman05@gmail.com>
2024-02-22 11:43:40 +0600

    firebase configured

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBeUSElZ6yhw9d-O7AApWGqhYHfmsnR6dc",
+  authDomain: "bytebazaar-82b09.firebaseapp.com",
+  projectId: "bytebazaar-82b09",
+  storageBucket: "bytebazaar-82b09.appspot.com",
+  messagingSenderId: "326255446943",
+  appId: "1:326255446943:web:8ba6a7bced3fcd338a4baa"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
